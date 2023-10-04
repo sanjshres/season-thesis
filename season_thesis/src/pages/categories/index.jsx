6837c1d6bf44bb2733e22b3986c6da2cf8c6dbcc
sanjshrestha <sanjay.shrestha@fusemachines.com>
2023-10-04 11:56:32 +0545

    added boostrap table

---
+++ season_thesis/src/pages/categories/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BootstrapTable from '../../components/boostrapTable';
+
+const Categories = () => {
+  return (
+    <div className="">
+      <BootstrapTable/>
+    </div>
+  );
+};
+
+export default Categories;
---
+++ season_thesis/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to Season Thesis</h1>
+      <p>This is your home page.</p>
+      <a href="/category">Categories</a>
+    </div>
+  );
+};
+
+export default Home;
